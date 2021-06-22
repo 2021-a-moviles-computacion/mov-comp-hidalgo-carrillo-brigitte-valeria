a6968571b20a4aa4f0c46af4bdc9f6c320bf0bed
valeriah_cv <valeriahidalgo50@gmail.com>
2021-06-22 13:58:15 -0500

    ciclo de vida
    Sumar con un botón y hacer que no se pierda el valor cuando el teléfono cambia de posición.

--- android_3/app/src/main/java/com/ferrifrancis/andorid/MainActivity.kt
+++ android_3/app/src/main/java/com/ferrifrancis/andorid/MainActivity.kt
@@ -3,12 +3,16 @@ package com.ferrifrancis.andorid
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //LOG
+        //TIPOS: informacion, debug, warning
+        Log.i("ciclo-vida", "onCreate")
 
         val botonIrACicloVida = findViewById<Button>(
             R.id.btn_ciclo_vida
@@ -17,6 +21,31 @@ class MainActivity : AppCompatActivity() {
         botonIrACicloVida.setOnClickListener { //escucho el boton
             abrirCicloVida()
         }
+    }
+
+    override  fun onStart()
+    { super.onStart()
+        Log.i("ciclo-vida", "onStart")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i("ciclo-vida", "onRestart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("ciclo-vida", "onResume")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("ciclo-vida", "onDestroy")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("ciclo-vida", "onPause")
     }
 
     //funcion para abrir pantalla ciclo de vida
