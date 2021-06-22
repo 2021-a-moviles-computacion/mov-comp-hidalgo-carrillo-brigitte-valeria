bca632840018947127cafa816436595bcb6a6ef7
valeriah_cv <valeriahidalgo50@gmail.com>
2021-06-22 11:03:07 -0500

    ciclo vida

---
+++ android_3/app/src/main/java/com/ferrifrancis/andorid/ACicloVida.kt
@@ -0,0 +1,11 @@
+package com.ferrifrancis.andorid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ACicloVida : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_a_ciclo_vida)
+    }
+}
--- android_3/app/src/main/java/com/ferrifrancis/andorid/MainActivity.kt
+++ android_3/app/src/main/java/com/ferrifrancis/andorid/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.ferrifrancis.andorid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val botonIrACicloVida = findViewById<Button>(
+            R.id.btn_ciclo_vida
+        //R.id : busca los recursos que he creando
+        )
+        botonIrACicloVida.setOnClickListener { //escucho el boton
+            abrirCicloVida()
+        }
+    }
+
+    //funcion para abrir pantalla ciclo de vida
+    fun abrirCicloVida()
+    {
+        //para que se abra la página debo hacer un intent
+        val intentExplicito = Intent(
+            this,
+            ACicloVida:: class.java
+        )
+        //startActivity para que aparezca la nueva actividad
+        this.startActivity(intentExplicito)
     }
 }
