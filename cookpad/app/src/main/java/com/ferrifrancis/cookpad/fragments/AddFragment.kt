c32a94747a515d345d39166fde47318422a19b83
valeriah_cv <valeriahidalgo50@gmail.com>
2021-08-05 11:16:44 -0500

    chip listener added

--- cookpad/app/src/main/java/com/ferrifrancis/cookpad/fragments/AddFragment.kt
+++ cookpad/app/src/main/java/com/ferrifrancis/cookpad/fragments/AddFragment.kt
@@ -5,8 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 import com.ferrifrancis.cookpad.R
-
+import com.google.android.material.chip.Chip
+import kotlinx.android.synthetic.main.fragment_add.*
 
 
 /**
@@ -19,9 +22,7 @@ class AddFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
 
-        }
     }
 
     override fun onCreateView(
@@ -32,5 +33,14 @@ class AddFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_add, container, false)
     }
 
+    override fun onResume() {
+        super.onResume()
+        chip_receta.setOnClickListener {
+            Toast.makeText(context, "This is chip1", Toast.LENGTH_SHORT).show()
+        }
+        chip_truco.setOnClickListener {
+            Toast.makeText(context, "This is chip2", Toast.LENGTH_SHORT).show()
+        }
+    }
 
 }
