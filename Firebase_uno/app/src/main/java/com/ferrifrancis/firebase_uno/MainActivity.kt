ace2144329079e2212affe5d138022001532531e
valeriah_cv <valeriahidalgo50@gmail.com>
2021-08-24 14:27:32 -0500

    hasta colección usuarios

--- Firebase_uno/app/src/main/java/com/ferrifrancis/firebase_uno/MainActivity.kt
+++ Firebase_uno/app/src/main/java/com/ferrifrancis/firebase_uno/MainActivity.kt
@@ -7,7 +7,11 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthAnonymousUpgradeException
 import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     val CODIGO_INICIO_SESION = 102
@@ -48,16 +52,48 @@ class MainActivity : AppCompatActivity() {
         when (requestCode) {
             CODIGO_INICIO_SESION -> {
                 if (resultCode == Activity.RESULT_OK) {
-                    val usuario = IdpResponse.fromResultIntent(data)
-                    if (usuario?.isNewUser == true) {
-                        Log.i("firebase-login", "Nuevo Usuario")
-                    } else {
-                        Log.i("firebase-login", "Usuario Antiguo")
+                    val usuario: IdpResponse? = IdpResponse.fromResultIntent(data)
+                    if(usuario != null) {
+
+                        if (usuario?.isNewUser == true) {
+                            Log.i("firebase-login", "Nuevo Usuario")
+                            registrarUsuarioPorPrimeraVez(usuario)
+                        } else {
+                            Log.i("firebase-login", "Usuario Antiguo")
+                        }
                     }
                 } else {
                     Log.i("firebase-login", "El usuario cancelo")
                 }
             }
+        }
+    }
+
+    fun registrarUsuarioPorPrimeraVez(usuario: IdpResponse )
+    {
+        val usuarioLogeado = FirebaseAuth.getInstance().getCurrentUser()
+        if(usuario.email !=null && usuarioLogeado != null)
+        {
+            Log.i("firebase-firestore","email y usuario no null")
+            //roles : ["usuario","admin" ]
+            val db = Firebase.firestore
+            val rolesUsuario = arrayListOf("usuario")
+            val nuevoUsuario = hashMapOf<String, Any>(
+                "roles" to rolesUsuario,
+            "uid" to usuarioLogeado.uid
+            )
+            val identificadorUsuario = usuario.email
+            db.collection("usuario")
+                .add(nuevoUsuario)
+                .addOnSuccessListener {
+                    Log.i("firebase-firestore", "Se creó")
+                }
+                .addOnFailureListener {
+                 Log.i("firebase-firestore", "Falló")
+                }
+        }
+        else {
+            Log.i("firebase-login","ERROR")
         }
     }
 }
